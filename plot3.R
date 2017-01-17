e3e173d7d0e5c4a2579f308fc8bbcbc55d11370c
engAnt <codersellu@outlook.com>
2017-01-17 15:17:16 -0600

    enhance plots

--- plot3.R
+++ plot3.R
@@ -8,8 +8,7 @@ with(useful_data, plot(Time, Sub_metering_1,
 with(useful_data, lines(Time, Sub_metering_2, col = "red"))
 with(useful_data, lines(Time, Sub_metering_3, col = "blue"))
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       lwd = 2, lty = 1, cex = 0.75,
-       col = c("black", "blue", "red"))
+       lty = 1, col = c("black", "blue", "red"))
 
 dev.copy(png, file = "plot3.PNG") 
 dev.off() 
--- plot4.R
+++ plot4.R
@@ -1,26 +1,22 @@
 
 source("main.R")
 
-par(mfrow = c(2, 2))
+par(mfrow = c(2, 2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(useful_data, {
-     plot(Time, Global_active_power, 
-          main = "", xlab = "",
+     plot(Time, Global_active_power, main = "", xlab = "",
           ylab = "Global Active Power (kilowatts)", type = "l")
      
      plot(Time, Voltage, 
           main = "", xlab = "datetime",
           ylab = "Voltage", type = "l")
      
-     plot(Time, Sub_metering_1, 
-          main = "", xlab = "",
-          ylab = "Energy sub metering", type = "l")
+     plot(Time, Sub_metering_1, main = "", xlab = "", ylab = "Energy sub metering", type = "l")
      lines(Time, Sub_metering_2, col = "red")
      lines(Time, Sub_metering_3, col = "blue")
      legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       lwd = 2, lty = 1, col = c("black", "blue", "red"), cex = 0.5, bty = "n")
+       lty = 1, col = c("black", "blue", "red"), cex = 0.8, bty = "n")
      
-     plot(Time, Global_reactive_power, 
-          main = "", xlab = "datetime",
+     plot(Time, Global_reactive_power, main = "", xlab = "datetime",
           ylab = "Global_reactive_power", type = "l")
 })
 
